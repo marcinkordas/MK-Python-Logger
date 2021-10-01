e6831203bd6bd6a8d50649c3836e4cb3e1e53c6f
Marcin Kordas <marcin.kordas@gmail.com>
2021-10-01 14:56:20 +0200

    Updated dir creation +  erase

--- Logger_CLASS.py
+++ Logger_CLASS.py
@@ -3,16 +3,28 @@ import numpy
 import os
 
 class Logger():
-    def __init__(self, filename, path = os.getcwd()):
+    def __init__(self, filename, directory, parent_dir = os.getcwd()):
         self.filename = filename
-        self.path = path
+        self.directory = directory
+          
+        # Parent Directory path
+        self.parent_dir = parent_dir
+
+        # Path
+        self.path = os.path.join(parent_dir, self.directory)
+        
+        if os.path.exists(self.path) == False: 
+            os.mkdir(self.path)
+            print("Directory '% s' created" % self.directory)
+        else:
+            self.erase()
         
     def append(self, *argv, **kwargs):
         self.append_end_NL(argv)    
     
     def append_end_NL(self, *argv, **kwargs):
         access_method = 'a'
-        log = open(self.path + '/' + self.filename, access_method)
+        log = open(os.path.join(self.path, self.filename), access_method)
         line = []
         for arg in argv:
             if type(arg) == list or type(arg) == numpy.array or type(arg) == numpy.ndarray:
@@ -25,7 +37,7 @@ class Logger():
         
     def append_start_NL(self, *argv, **kwargs):
         access_method = 'a'
-        log = open(self.path + '/' + self.filename, access_method)
+        log = open(os.path.join(self.path, self.filename), access_method)
         log.write("\r\n")
         line = []
         for arg in argv:
@@ -37,7 +49,7 @@ class Logger():
         log.close
         
     def append_inline(self, *argv, access_method = 'a', **kwargs):
-        log = open(self.path + '/' + self.filename, access_method)
+        log = open(os.path.join(self.path, self.filename), access_method)
         line = []
         for arg in argv:
             if type(arg) == list or type(arg) == numpy.array or type(arg) == numpy.ndarray:
@@ -51,14 +63,15 @@ class Logger():
         if permission != 'y':
             permission = input("Type 'y' to permit file erase {}\n".format(self.filename))
         if permission == 'y':
-            open(self.path + '/' + self.filename, "w").close()
+            open(os.path.join(self.path, self.filename), "w").close()
+            # open(self.path + '/' + self.filename, "w").close()
         else: 
             print("User skipped file erase")
 
 
 if __name__ == "__main__":
-    log = Logger(filename = 'log.csv')
+    log = Logger(filename = 'log.csv', directory ='LOG')
     print(log.filename)
     print(log.path)
-    log.erase()
-    log.append_end_NL([1,2,3,5],"test", "ciastka", filename = 'log.csv', access_method = 'a')
+    # log.erase()
+    log.append_end_NL([1,2,3,5],"test", "ciastka", filename = 'log.csv', directory ='LOG', access_method = 'a')
